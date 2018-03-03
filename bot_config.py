9fc95fe4430cfbccaed3366a1fe1703b7dbb3e95
Adhar <35442778+adhar-m@users.noreply.github.com>
2018-03-03 08:34:26 -0800

    Added config files with placeholder values

---
+++ bot_config.py
@@ -0,0 +1,26 @@
+# Enter the name of the subreddit(s) you want to monitor.
+# Multiple subs can be monitored by entering: "sub1+sub2+sub3"
+subreddit_name = "test"
+
+# Enter the search term you want to monitor (eg: your company's name)
+search_term = "changeThis"
+
+# The next few options are booleans. True to enable, False to disable
+
+# Conduct sentiment analysis?
+get_sentiment = True
+
+# Display subjectivity with sentiment?
+get_subjectivity = True
+
+# Fetch Comment Score?
+get_score = True
+
+# Fetch number of up/down votes for Score?
+get_score_breakdown = True
+
+# Message you the results?
+get_messages = True
+
+# Create a digest? If True, how many messages per digest?
+get_digest = (True, 10)
---
+++ praw_config.py
@@ -0,0 +1,4 @@
+# Change the placeholder values to instantiate your bot
+client_id='placeholder'
+client_secret='placeholder'
+user_agent='placeholder'
