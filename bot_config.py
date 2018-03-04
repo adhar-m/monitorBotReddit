620e2f8affe51fab7e1a3e6836e91de553cf3931
Adhar <35442778+adhar-m@users.noreply.github.com>
2018-03-03 23:09:12 -0800

    Update bot_config.py
    Added logging for comments already seen

--- bot_config.py
+++ bot_config.py
@@ -1,9 +1,12 @@
 # Enter the name of the subreddit(s) you want to monitor.
 # Multiple subs can be monitored by entering: "sub1+sub2+sub3"
-subreddit_name = "test"
+subreddit_name = "Change Me!"
 
 # Enter the search term you want to monitor (eg: your company's name)
-search_term = "changeThis"
+search_term = "Change Me!"
+
+# Name of file storing comments already seen
+comments_seen = "comments_seen.txt"
 
 # The next few options are booleans. True to enable, False to disable
 
