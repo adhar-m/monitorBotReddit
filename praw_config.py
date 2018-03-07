e9886cba53a69b34aeabd3c7ad28ae5c2c631ec8
adhar-m <adhar.maheshwari@gmail.com>
2018-03-07 03:03:54 -0800

    Added username and password placeholders

--- praw_config.py
+++ praw_config.py
@@ -1,4 +1,6 @@
-# Change the placeholder values to instantiate your bot
-client_id='placeholder'
-client_secret='placeholder'
-user_agent='placeholder'
+# Change the placeholder values to instantiate and authenticate your bot
+client_id = 'placeholder'
+client_secret = 'placeholder'
+user_agent = 'placeholder'
+username = 'placeholder'
+password = 'placeholder'
